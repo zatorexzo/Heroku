aee6c49b515af307bed75a463746cc7333f066d2
zatorexzo <zatorexzo@gmail.com>
2022-03-04 13:50:10 -0500

    pass flake8 app.py

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
+# To change git
+
 from flask import Flask, render_template
-#To change git
 app = Flask(__name__)
+
 
 @app.route("/")
 def index():
-    
     # Load current count
     f = open("count.txt", "r")
     count = int(f.read())
@@ -20,6 +21,7 @@ def index():
 
     # Render HTML with count variable
     return render_template("index.html", count=count)
+
 
 if __name__ == "__main__":
     app.run()
